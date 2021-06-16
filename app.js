4c1a71d8ae055085c2eaa86d0670f9d0e0c15836
250391Ivan <25ivan.pillado@gmail.com>
2021-06-16 18:41:53 -0500

    Creando los  modelo y  probando uuid

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  const colors  = require('colors');
  /* const { MostrarMenu,Pausar } = require('./helpers/mensajes'); */
  const {MenuInquiri,Pausar} = require('./helpers/inquirir');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
  
  
 
@@ -10,12 +12,22 @@ const main= async()=>{
 
     let  opcion = '';
     do {
-        
-        opcion = await MenuInquiri();
-        
-        console.log({opcion});
-        await Pausar();
 
+        
+        const  tarea = new Tarea('guardar en git');
+        const  tareas  = new Tareas();
+        console.log(tarea);
+        console.log(tareas);
+        tareas._listado[tarea.id]= tarea;
+
+        console.log(tareas);
+        
+        
+        await Pausar();
+        
+       /* 
+        console.log({opcion});
+ */
       /*   opcion = await MostrarMenu(); */
      /*    if (opcion !== '0') {
             await Pausar();
